8c3ddc4064dbd837202ae71c2803451f70449bfc
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-07-16 11:22:11 +0530

    Code Improved.

--- FirstProgram.cs
+++ FirstProgram.cs
@@ -1,18 +1,20 @@
+// The Following are the .Net Framework Namespaces.
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSharpTutorials
+// namespace name
+namespace CSharpTutorials 
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string message = "Hello World!!";
+            string message = "Hello World!!";// message is our variable and Hello World is value of the variable.
 
-            Console.WriteLine(message);
+            Console.WriteLine(message); // Method to display values on the console.
         }
     }
 }
