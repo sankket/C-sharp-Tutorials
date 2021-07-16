5fc7af817e2b370b49d10aec57c4b3066ff5266f
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-07-16 11:46:02 +0530

    Code Updated.

--- Class.cs
+++ Class.cs
@@ -2,12 +2,14 @@
 // it has some methods,properties, fields and events.
 // A class enables you to create your custom types by grouping variables of other types, methods, and events.
 
-public class MyClass
+// public is access modifier, there are 4 access modifier : public, private, protected, and internal in c#.
+public class MyClass 
 {
     public string  myField = string.Empty;
 
     public MyClass()
     {
+        // The constructor is called when you create an instance of the class.
     }
 
     public void MyMethod(int parameter1, string parameter2)
@@ -16,6 +18,7 @@ public class MyClass
                                                     parameter1, parameter2);
     }
 
+    // A property can be defined using getters and setters. it is an auto implemented property.
     public int MyAutoImplementedProperty { get; set; }
 
     private int myPropertyVar;
