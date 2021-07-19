46c3a7a0a584923e81cdd338ff1c2df1cf3377f1
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-07-19 12:27:01 +0530

    Code Improved.

--- SwitchCases.cs
+++ SwitchCases.cs
@@ -1,20 +1,13 @@
 //The switch statement can be used instead of if else statement when you want to test a variable against three or more conditions.
+//The break, return, or goto keyword is used to exit the program control from a switch case.
+int x = 125;
 
-int x = 10;
-
-switch (x) //x is the matched expression. the result is specified in the curly braces.
+switch (x % 2)
 { 
-    case 5:
-        Console.WriteLine("Value of x is 5");
+    case 0:
+        Console.WriteLine($"{x} is an even value");
         break;
-        //The break, return, or goto keyword is used to exit the program control from a switch case.
-    case 10:
-        Console.WriteLine("Value of x is 10");
-        break;
-    case 15:
-        Console.WriteLine("Value of x is 15");
-        break;
-    default:
-        Console.WriteLine("Unknown value");
+    case 1:
+        Console.WriteLine($"{x} is an odd Value");
         break;
 }
