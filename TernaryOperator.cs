845dfdaf58354664dc32fcd28db9519a6d2cd1a9
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-07-19 12:21:38 +0530

    Code Updated.

--- TernaryOperator.cs
+++ TernaryOperator.cs
@@ -1,1 +1,9 @@
+// the ternary operatoer is if else statement in one line.
+// its used as "?:".\
+// condition ? statement1 : statement2
 
+int x = 10, y = 100;
+
+var result = x > y ? "x is greater than y" : "x is less than y";
+
+Console.WriteLine(result);
