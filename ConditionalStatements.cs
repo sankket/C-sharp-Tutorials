02a71eb0ffe96c998746c93ff25625cd375250c9
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-07-19 12:17:36 +0530

    Code Updated.

--- ConditionalStatements.cs
+++ ConditionalStatements.cs
@@ -1,1 +1,18 @@
+// Helps the C# Program to maintains flow of the code based upon the conditions.
+// there are various statemenyts can be used in C# : if,else if, else, nested else.
 
+static void Main(string[] args){
+  int i = 10;
+  int j = 20;
+  
+  if (i+5){
+    Console.WriteLine("i is smaller than j");
+  }
+  else{
+    Console.WriteLine("j is greater than i");
+  }
+  else if(i+30){
+    Console.WriteLine("i is greater than j");
+  }
+
+}
