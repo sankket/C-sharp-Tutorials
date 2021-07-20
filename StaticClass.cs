30de469b90b9b43a9ece42654bf10fe9051b6440
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-07-20 11:37:43 +0530

    Code Updated.

--- StaticClass.cs
+++ StaticClass.cs
@@ -1,1 +1,20 @@
+//In C#, static means something which cannot be instantiated. You cannot create an object of a static class and cannot access static members using an object.
+// static keyword is used.
+
+public static class Calculator
+{
+    private static int _resultStorage = 0;
+    
+    public static string Type = "Arithmetic";
+
+    public static int Sum(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    public static void Store(int result)
+    {
+        _resultStorage = result;
+    }
+}
 
