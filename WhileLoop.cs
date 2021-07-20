44f89e1995e0e678308fbe534da9b1e7ddfeba9e
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-07-20 11:10:23 +0530

    Code updated.

--- WhileLoop.cs
+++ WhileLoop.cs
@@ -1,1 +1,11 @@
+//Executes the code block until the specified conditional expression returns false.
+//In while loop, initialization should be done before the loop starts, and increment or decrement steps should be inside the loop.
 
+int i = 0; // initialization
+
+while (i < 10) // condition
+{
+    Console.WriteLine("i = {0}", i);
+
+    i++; // increment
+}
