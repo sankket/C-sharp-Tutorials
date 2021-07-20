863ca8d40108e1baf2544121828514cf95bd3843
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-07-20 11:33:35 +0530

    Code Updated.

--- PartialClass.cs
+++ PartialClass.cs
@@ -1,1 +1,9 @@
+// split the implementation of the class into multiple .cs files.
+// partial keyword is used in this case.
+// one file contains one implementation of class and other can have different implemenrtation of it.
 
+public partial class Employee
+{
+    public int EmpId { get; set; }
+    public string Name { get; set; }
+}
