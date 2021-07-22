85ce10f636f4ad18d8d3ed5e502ce34dbaa36953
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-07-22 13:32:08 +0530

    Code Updated.

--- Arrays.cs
+++ Arrays.cs
@@ -1,1 +1,8 @@
+//array is used to store multiple literal values.
+//An array is the data structure that stores a fixed number of literal values (elements) of the same data type. Array elements are stored contiguously in the memory.
 
+var[] evenNums = new int[5]{ 2, 4, 6, 8, 10 }; 
+
+Console.WriteLine(evenNums[1]);
+
+string[] cities = new string[3]{ "Mumbai", "London", "New York" };
