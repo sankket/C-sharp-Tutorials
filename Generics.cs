6ebe43a835a2c7fb98f4e5bc227a227ebba322fa
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-07-23 11:26:49 +0530

    Code Updated.

--- Generics.cs
+++ Generics.cs
@@ -1,1 +1,9 @@
+//generic means not specific to a particular data type.
+//A generic type is declared by specifying a type parameter in an angle brackets after a type name, e.g. TypeName<T> where T is a type parameter.
 
+class DataStore<T>
+{
+    public T Data { get; set; }
+}
+
+DataStore<string> store = new DataStore<string>();
