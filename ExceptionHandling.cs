a45aafce55b41bca4f276b5d67895cec836df89e
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-07-26 11:52:57 +0530

    Code Updated.

--- ExceptionHandling.cs
+++ ExceptionHandling.cs
@@ -1,1 +1,26 @@
+// Exceptions in the application must be handled to prevent crashing of the program and unexpected result. its an unexpected error which disrupts the normal execution of code.
+// C# provides built-in support to handle the exception using try, catch & finally blocks.
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Enter a number: ");
+
+            var num = int.parse(Console.ReadLine());
+
+            Console.WriteLine($"Squre of {num} is {num * num}");
+        }
+        catch
+        {
+            Console.Write("Error occurred.");
+        }
+        finally
+        {
+            Console.Write("Re-try with a different number.");
+        }
+    }
+}
 
