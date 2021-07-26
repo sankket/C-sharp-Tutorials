d02fe560b070c3086a7e4ac9575ab664295d85f5
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-07-26 11:41:58 +0530

    Code Updated.

--- Tuple.cs
+++ Tuple.cs
@@ -1,1 +1,15 @@
+// A tuple is a data structure that contains a sequence of elements of different data types.
+// A tuple can only include a maximum of eight elements.
 
+tatic void Main(string[] args)
+{
+    var person = Tuple.Create(1, "Sanket", "Godbole");
+    DisplayTuple(person);
+}
+
+static void DisplayTuple(Tuple<int,string,string> person)
+{
+    Console.WriteLine($"Id = { person.Item1}");
+    Console.WriteLine($"First Name = { person.Item2}");
+    Console.WriteLine($"Last Name = { person.Item3}");
+}
