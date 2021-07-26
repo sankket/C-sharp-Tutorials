24519ad8013d7c55629cd800e4910c85ae119e3c
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-07-26 12:42:30 +0530

    Code Updated.

--- Throw.cs
+++ Throw.cs
@@ -1,1 +1,24 @@
+//An exception can be raised manually by using the throw keyword.
+static void Main(string[] args)
+{
+    Student std = null;
 
+    try
+    {
+        PrintStudentName(std);
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine(ex.Message );
+    }                      
+
+    Console.ReadKey();
+}
+
+private static void PrintStudentName( Student std)
+{
+    if (std  == null)
+        throw new NullReferenceException("Student object is null.");
+        
+    Console.WriteLine(std.StudentName);
+}
