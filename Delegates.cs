d017f94208817c8bd7fa0c6ce1089d6416e9cc7b
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-07-30 11:28:56 +0530

    Code Updated.

--- Delegates.cs
+++ Delegates.cs
@@ -1,1 +1,17 @@
+// if we want to pass function as a parameter.
+// The delegate is a reference type data type that defines the method signature.
 
+public delegate void MyDelegate(string msg); // declare a delegate
+
+// set target method
+MyDelegate del = new MyDelegate(MethodA);
+// or 
+MyDelegate del = MethodA; 
+// or set lambda expression 
+MyDelegate del = (string msg) =>  Console.WriteLine(msg);
+
+// target method
+static void MethodA(string message)
+{
+    Console.WriteLine(message);
+}
