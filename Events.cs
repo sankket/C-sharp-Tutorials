c0cf6c7f34dfd60b90d9c6e0742c789508c46b93
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-08-02 11:25:39 +0530

    Code Updated.

--- Events.cs
+++ Events.cs
@@ -1,1 +1,11 @@
+// An event is a notification sent by an object to signal the occurrence of an action.
+// class who raises an event is publisher and who recieves it is subscriber. Basically it follows pub-sub model.
 
+//Declaring the event
+public delegate void Notify();  // delegate
+                    
+public class ProcessBusinessLogic
+{
+    public event Notify ProcessCompleted; // event
+
+}
