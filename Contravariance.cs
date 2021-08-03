e5eedc399e2f43919c55d79aa724d109e355759b
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-08-03 11:23:33 +0530

    Code Updated.

--- Contravariance.cs
+++ Contravariance.cs
@@ -1,1 +1,32 @@
+// Contravariance is applied to the parameters.
+// Cotravariance allows a method with the parameter of a base class to be assigned to a delegate that expects the parameter of a derived class. 
 
+delegate Small covarDel(Big mc);
+
+class Program
+{
+    static Big Method1(Big bg)
+    {
+        Console.WriteLine("Method1");
+        return new Big();
+    }
+    static Small Method2(Big bg)
+    {
+        Console.WriteLine("Method2");
+        return new Small();
+    }
+
+    static Small Method3(Small sml)
+    {
+        Console.WriteLine("Method3");
+        
+        return new Small();
+    }
+    static void Main(string[] args)
+    {
+        covarDel del = Method1;
+        del += Method2;
+        del += Method3;
+
+        Small sm = del(new Big());
+}
