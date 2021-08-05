423aea0d172bab25d9e2e5e91866d0b08a551021
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-08-05 11:11:52 +0530

    Code Updated.

--- ExtensionMethod.cs
+++ ExtensionMethod.cs
@@ -1,1 +1,17 @@
+// Extension methods allow you to inject additional methods without modifying, deriving or recompiling the original class, struct or interface.
+using ExtensionMethods;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i = 10;
+
+        bool result = i.IsGreaterThan(100); 
+
+        Console.WriteLine(result);
+    }
+}
+
+// here the IsGreaterThan method is extension method.
+// the extension method has special intellisense symbol in the IDE.
