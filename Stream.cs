e36d5959bc2dd63ae4a75dd6d30973dccc77d2bf
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-08-06 12:51:59 +0530

    File Updated.

--- Stream.cs
+++ Stream.cs
@@ -1,1 +1,7 @@
+//standard IO (Input/Output) classes to read/write from different sources like files, memory, network, isolated storage, etc.Stream is an abstract class for transfering bytes from different sources. It is base class for all other class that reads\writes bytes to different sources.
 
+// Stream: System.IO.Stream is an abstract class that provides standard methods to transfer bytes (read, write, etc.) to the source.
+
+//FileStream reads or writes bytes from/to a physical file, whether it is a .txt, .exe, .jpg, or any other file.
+
+// MemoryStream: MemoryStream reads or writes bytes that are stored in memory.
