3354c74f5787adce3cef5dadd1a4cd18b4439105
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-08-09 12:21:30 +0530

    Code Improved.

--- File.cs
+++ File.cs
@@ -10,3 +10,27 @@ string dummyLines = "This is first line." + Environment.NewLine +
 File.AppendAllLines(@"C:\DummyFile.txt", dummyLines.Split(Environment.NewLine.ToCharArray()).ToList<string>());
 
 //the above code will create the file with the 3 lines.
+
+//Check whether file is exists or not at particular location
+bool isFileExists = File.Exists(@"C:\ DummyFile.txt"); // returns false
+
+//Copy DummyFile.txt as new file DummyFileNew.txt
+File.Copy(@"C:\DummyFile.txt", @"D:\NewDummyFile.txt");
+
+//Get when the file was accessed last time 
+DateTime lastAccessTime = File.GetLastAccessTime(@"C:\DummyFile.txt");
+
+//get when the file was written last time
+DateTime lastWriteTime = File.GetLastWriteTime(@"C:\DummyFile.txt");
+
+// Move file to new location
+File.Move(@"C:\DummyFile.txt", @"D:\DummyFile.txt");
+
+//Open file and returns FileStream for reading bytes from the file
+FileStream fs = File.Open(@"D:\DummyFile.txt", FileMode.OpenOrCreate);
+
+//Open file and return StreamReader for reading string from the file
+StreamReader sr = File.OpenText(@"D:\DummyFile.txt");
+    
+//Delete file
+File.Delete(@"C:\DummyFile.txt");
