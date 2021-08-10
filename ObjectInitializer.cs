4600e412d9853dc106b7ba066ee62ea70cec98d4
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-08-10 14:44:46 +0530

    Code Updated.

--- ObjectInitializer.cs
+++ ObjectInitializer.cs
@@ -1,1 +1,21 @@
+// Object initializers allow you to assign values to the fields or properties at the time of creating an object without invoking a constructor.
 
+public class Student
+{
+    public int StudentID { get; set; }
+    public string StudentName { get; set; }
+    public int Age { get; set; }
+    public string Address { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student std = new Student() { StudentID = 06, 
+                                      StudentName = "Sanket Godbole", 
+                                      Age = 22, 
+                                      Address = "Munich"   
+                                    };
+    }
+}
