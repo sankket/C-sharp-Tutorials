3f6c2e1959c57641f512b475af524e49534c3cde
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-08-11 12:02:03 +0530

    Code Updated.

--- SumofTwo.cs
+++ SumofTwo.cs
@@ -1,1 +1,10 @@
-
+//Print the sum of two numbers.
+using System;
+class SumOfTwo {
+  static void Main() {
+      int num1 = 5;
+      int num2 = 10;
+      
+      Console.WriteLine(num1 + num2);
+  }
+}
