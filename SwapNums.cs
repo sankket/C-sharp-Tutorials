2fbb43e2fb280b7ffc25903dac6ae935ab2fea60
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-08-16 12:11:20 +0530

    Code Updated.

--- SwapNums.cs
+++ SwapNums.cs
@@ -1,1 +1,18 @@
-
+using System;
+class SwapNums {
+  static void Main() {
+    int num1, num2, temp;
+    Console.Write("Enter the First Number : ");
+    num1 = int.Parse(Console.ReadLine());
+    Console.Write("Enter the Second Number : ");
+    num2 = int.Parse(Console.ReadLine());
+    
+    temp = num1;
+    num1 = num2;
+    num2 = temp;
+    
+    Console.Write("First number : ",num1);
+    Console.Write("Second number : ",num2);
+    Console.Read();
+  }
+}
