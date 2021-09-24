17e964f84e5abc0ea84641b95e64662a51c66162
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-09-24 11:24:10 +0530

    Code Updated.

--- Age.cs
+++ Age.cs
@@ -1,1 +1,14 @@
+using System;
+public class Exercise11
+{
+  public static void Main()
+  {
+    int age;
+    Console.Write("Enter your age ");
+    age = Convert.ToInt32(Console.ReadLine());
+    Console.Write("You look younger than {0} ",age);
+  }
+}
 
+
+//Program just print out the age of the person with some comment
