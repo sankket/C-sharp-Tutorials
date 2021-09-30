5aa10d0a995036697e9c7f7490f9dfd932b17a3a
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-09-30 11:19:45 +0530

    Code Updated.

---
+++ CelciusToFeh.cs
@@ -0,0 +1,12 @@
+using System;
+public class Exercise14
+{
+   public static void Main( )
+    {
+        Console.Write("Enter the amount of Celsius: "); 
+        int celsius = Convert.ToInt32(Console.ReadLine());
+ 
+        Console.WriteLine("Kelvin = {0}", celsius + 273);
+        Console.WriteLine("Fahrenheit = {0}", celsius * 18 / 10 + 32);
+    }
+}
