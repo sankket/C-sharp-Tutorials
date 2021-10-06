c5f32eb552f5f52d70a16c884b6791bd4680b451
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-10-06 11:26:17 +0530

    Code Updated.

--- LetterExchange.cs
+++ LetterExchange.cs
@@ -1,1 +1,17 @@
+using System;
+using System.Text;
 
+public class LetterExchange {
+  static void Main(string[] args)
+        {
+           string str; 
+           int l= 0;
+           Console.Write("Input a string : ");
+           str = Console.ReadLine();
+           if (str.Length>=1)
+           {
+           var s = str.Substring(0,1);
+           Console.WriteLine(s + str + s);
+           }
+        }
+}
