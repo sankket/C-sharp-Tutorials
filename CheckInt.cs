4ee3fc7281b1cd682c5fb2745a87a423996d4f7f
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-10-25 15:42:36 +0530

    Code Updated.

--- CheckInt.cs
+++ CheckInt.cs
@@ -1,1 +1,14 @@
+using System;
+using System.Collections.Generic;
 
+public class CheckInt {
+  static void Main(string[] args)
+        {
+           Console.WriteLine("\nInput first integer:");  
+           int x = Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine("Input second integer:");  
+           int y = Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine("Check if one is negative and one is positive:");
+           Console.WriteLine((x < 0 && y > 0) || (x > 0 && y < 0));
+        }
+}
