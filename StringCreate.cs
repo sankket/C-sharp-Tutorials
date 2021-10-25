45edf789078244c0401a493f5532f383d14e549d
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-10-25 15:37:22 +0530

    Code Updated.

--- StringCreate.cs
+++ StringCreate.cs
@@ -1,1 +1,16 @@
+using System;
+using System.Collections.Generic;
 
+public class CreateString {
+  static void Main(string[] args)
+        {
+            Console.WriteLine(first_last("Sanket"));
+            Console.WriteLine(first_last("Python"));
+            Console.WriteLine(first_last("India"));
+        }
+       public static string first_last(string ustr)
+        {
+            return ustr.Length > 1
+                ? ustr.Substring(ustr.Length - 1) + ustr.Substring(1, ustr.Length - 2) + ustr.Substring(0, 1) : ustr;
+        }
+}
